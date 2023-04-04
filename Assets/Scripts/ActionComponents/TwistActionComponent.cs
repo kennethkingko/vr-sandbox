3ddb6a93cd1e9045dbf158bc703cfecc1b99368c
joan <joan.lee@obf.ateneo.edu>
2023-04-04 16:07:07 +0800

    Implement Twist Action

---
+++ Assets/Scripts/ActionComponents/TwistActionComponent.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwistActionComponent : BaseActionComponent
+{
+
+    [SerializeField] GameObject interactingObject;
+    public float distance;
+    public float requiredAngle;
+    [SerializeField] float angle;
+    [SerializeField] Transform entryTransform;
+    float zAngle;
+    
+    void Start()
+    {
+        actionCollider = gameObject.GetComponent<Collider>();
+        interactingObject = null;        
+    }
+
+    public override void Update()
+    {
+        if (!isCompleted && interactingObject != null) CheckIfCompleted();
+    }
+
+    public override void OnEntry(GameObject go)
+    {
+        if (go != null)
+        {
+            interactingObject = go;
+            Vector3 pos = go.transform.position;
+            Quaternion rot = go.transform.rotation;
+            zAngle = rot.z;
+            Debug.Log("Entry transform: " + pos + " " + rot);
+        }
+    }
+
+    //when is this used?
+    public bool CheckAction()
+    {
+        bool distanceCheck = false;
+
+        distanceCheck = Vector3.Distance(interactingObject.transform.position, gameObject.transform.position) <= distance;
+
+        return distanceCheck;
+    }
+
+    public override void CheckIfCompleted()
+    {   
+        Debug.Log("Check if completed");
+        if (interactingObject.GetComponent<ObjectStateManager>().currentState is ObjectGrabHoverState)
+        {
+            Vector3 pos = interactingObject.transform.position;
+            Quaternion rot = interactingObject.transform.rotation;
+
+            angle += rot.z - zAngle;
+            Debug.Log("Current angle: (" + angle + " - " + rot.z + " - "+ zAngle +")");
+        }
+        else
+        {
+            interactingObject = null;
+        }
+
+        if (angle >= requiredAngle)
+        {
+            isCompleted = true;
+            Debug.Log("Twisting action completed on " + gameObject.transform.parent.name);
+        }
+    }
+
+}
