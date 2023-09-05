48209811ef90ef33dbe2aac0cec3e2c2d664d716
joan lee <joan.lee@obf.ateneo.edu>
2023-09-05 13:23:55 +0800

    Improve calculation of forward movement, add stop point

--- Assets/Scripts/ActionComponents/TwistActionComponent.cs
+++ Assets/Scripts/ActionComponents/TwistActionComponent.cs
@@ -7,18 +7,23 @@ using UnityEngine;
 /// </summary>
 public class TwistActionComponent : BaseActionComponent
 {
-    // Values needed for the action to be completed
+    // Set in Inspector
     public float requiredAngle;
     public Vector3 boundsDirection;
+    public float stopMoveBuffer = 0;
+
+    //
     GameObject interactingObject;
-    float deltaAngle; 
+    float totalDeltaAngle;
     float deltaAngleBuffer;
     float interactingObjAngleInitial;
+    float movedInitial;
     float totalMoved;
    
     GameObject parentObject;
     float parentObjectLength;
     float parentObjAngleInitial;
+    Vector3 parentPosInitial;
    
     void Start()
     {
@@ -27,7 +32,8 @@ public class TwistActionComponent : BaseActionComponent
         parentObject = gameObject.transform.parent.gameObject;
         totalMoved = 0;
 
-
+        // to get length of object 
+        // if object at an angle, may need to manually input size instead
         if(boundsDirection.x > 0) {
             parentObjectLength = parentObject.GetComponent<Renderer>().bounds.size.x;
         }
@@ -61,6 +67,8 @@ public class TwistActionComponent : BaseActionComponent
             //Debug.Log("Entry transform: " + pos + " " + rot);        
             parentObjAngleInitial = parentObject.transform.eulerAngles.z;
             deltaAngleBuffer = 0;
+            parentPosInitial = parentObject.transform.position;
+            movedInitial = totalMoved;
         }
     }
 
@@ -74,13 +82,15 @@ public class TwistActionComponent : BaseActionComponent
            
             //delta angle gets the shortest angle between two angles (180, -180) E.g., what could be 190, will be -170
             //this handles within an interaction of action object entering before exiting receiver object, user's interaction is greater than 180 or less than -180
-            deltaAngle = deltaAngleBuffer + Mathf.DeltaAngle(rot.eulerAngles.z, interactingObjAngleInitial);
-            if (Mathf.DeltaAngle(rot.eulerAngles.z, interactingObjAngleInitial) > 175)
+            
+            float deltaAngle = Mathf.DeltaAngle(rot.eulerAngles.z, interactingObjAngleInitial);
+            totalDeltaAngle = deltaAngleBuffer + deltaAngle;
+            if (deltaAngle > 175)
             {
                 deltaAngleBuffer += 175;
                 interactingObjAngleInitial = rot.eulerAngles.z;
             }
-            else if (Mathf.DeltaAngle(rot.eulerAngles.z, interactingObjAngleInitial) < -175)
+            else if (deltaAngle < -175)
             {
                 deltaAngleBuffer -= 175;
                 interactingObjAngleInitial = rot.eulerAngles.z;
@@ -94,10 +104,21 @@ public class TwistActionComponent : BaseActionComponent
 
 
             // object forward movement
-            float addedZ = 0;
-            addedZ = (parentObjectLength/requiredAngle)*deltaAngle;
-            totalMoved += addedZ;
-            parentObject.transform.position += transform.forward*addedZ;
+                       
+            float move = -(parentObjectLength/requiredAngle)*totalDeltaAngle;
+            if (movedInitial + move > -stopMoveBuffer)
+            {   
+                Vector3 move3d = transform.forward*move;
+                parentObject.transform.position = new Vector3(
+                parentPosInitial.x + move3d.x,
+                parentPosInitial.y + move3d.y,
+                parentPosInitial.z + move3d.z); 
+                totalMoved = movedInitial + move;
+                
+                Debug.Log("Angle: " + totalDeltaAngle);
+                Debug.Log("Move: " + move + " - " + move3d);
+                Debug.Log("Ratios: " + (totalDeltaAngle/requiredAngle) + " - " + (move/parentObjectLength));
+            }
         }
         else
         {
