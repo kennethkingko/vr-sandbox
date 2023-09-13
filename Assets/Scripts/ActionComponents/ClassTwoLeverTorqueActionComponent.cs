7d48b1d308cbf2d76c100a78179265b870228554
joan <joan.lee@obf.ateneo.edu>
2023-09-13 17:28:06 +0800

    Re-add Lever Action Component

---
+++ Assets/Scripts/ActionComponents/ClassTwoLeverTorqueActionComponent.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The ClassTwoLeverTorqueActionComponent is an action component that mimics wrench-like action where torque is applied in a class-two level manner. Computation is made by checking the rotation along the y-component of the interacting object.
+/// </summary>
+public class ClassTwoLeverTorqueActionComponent : BaseActionComponent
+{
+    // Set in Inspector
+    public float requiredAngle;
+    public Vector3 boundsDirection;
+    public float stopMoveBuffer = 0;
+
+    //
+    GameObject interactingObject;
+    float totalDeltaAngle;
+    float deltaAngleBuffer;
+    float interactingObjAngleInitial;
+    float movedInitial;
+    float totalMoved;
+   
+    GameObject parentObject;
+    float parentObjectLength;
+    float parentObjAngleInitial;
+    Vector3 parentPosInitial;
+
+    public void Start()
+    {
+        actionCollider = gameObject.GetComponent<Collider>();
+        interactingObject = null;
+        parentObject = gameObject.transform.parent.gameObject;
+        totalMoved = 0;
+
+        // to get length of object 
+        // if object at an angle, may need to manually input size instead
+        if(boundsDirection.x > 0) {
+            parentObjectLength = parentObject.GetComponent<Renderer>().bounds.size.x;
+        }
+        else if(boundsDirection.y > 0) {
+            parentObjectLength = parentObject.GetComponent<Renderer>().bounds.size.y;
+        }
+        else if(boundsDirection.z > 0) {
+            parentObjectLength = parentObject.GetComponent<Renderer>().bounds.size.z;
+        }
+        else {
+            Debug.LogError("Must set bounds direction", gameObject.transform);
+        }        
+        //Debug.Log(gameObject.transform.parent.name + ": " + parentObjectLength);
+    }
+    
+    // Might need to be refactored for optimization
+    public override void Update()
+    {
+        if (!isCompleted && interactingObject != null) CheckIfCompleted();
+    }
+
+    // Function check to store the initial values of the interacting object
+    public override void OnEntry(GameObject go)
+    {
+        if (go != null)
+        {
+            interactingObject = go;
+            Vector3 pos = go.transform.position;
+            Quaternion rot = go.transform.rotation;
+            interactingObjAngleInitial = rot.eulerAngles.z;
+            //Debug.Log("Entry transform: " + pos + " " + rot);        
+            parentObjAngleInitial = parentObject.transform.eulerAngles.z;
+            deltaAngleBuffer = 0;
+            parentPosInitial = parentObject.transform.position;
+            movedInitial = totalMoved;
+            
+        }
+    }
+
+    public override void CheckIfCompleted()
+    {   
+        // Debug.Log("Check if completed");
+        if (interactingObject.GetComponent<ObjectStateManager>().currentState is ObjectGrabHoverState)
+        {
+            Quaternion rot = interactingObject.transform.rotation;
+           
+            //delta angle gets the shortest angle between two angles (180, -180) E.g., what could be 190, will be -170
+            //this handles within an interaction of action object entering before exiting receiver object, user's interaction is greater than 180 or less than -180
+            
+            float deltaAngle = Mathf.DeltaAngle(rot.eulerAngles.z, interactingObjAngleInitial);
+            totalDeltaAngle = deltaAngleBuffer + deltaAngle;
+            if (deltaAngle > 175)
+            {
+                deltaAngleBuffer += 175;
+                interactingObjAngleInitial = rot.eulerAngles.z;
+            }
+            else if (deltaAngle < -175)
+            {
+                deltaAngleBuffer -= 175;
+                interactingObjAngleInitial = rot.eulerAngles.z;
+            }
+           
+            // object rotation
+            parentObject.transform.eulerAngles = new Vector3(
+            parentObject.transform.eulerAngles.x,
+            parentObject.transform.eulerAngles.y,
+            parentObjAngleInitial + totalDeltaAngle);
+
+
+            // object forward movement
+                       
+            float move = -(parentObjectLength/requiredAngle)*totalDeltaAngle;
+            if (movedInitial + move > -stopMoveBuffer)
+            {   
+                Vector3 move3d = transform.forward*move;
+                parentObject.transform.position = new Vector3(
+                parentPosInitial.x + move3d.x,
+                parentPosInitial.y + move3d.y,
+                parentPosInitial.z + move3d.z); 
+                totalMoved = movedInitial + move;
+                
+                Debug.Log("Angle: " + totalDeltaAngle);
+                Debug.Log("Move: " + move + " - " + move3d);
+                Debug.Log("Ratios: " + (totalDeltaAngle/requiredAngle) + " - " + (move/parentObjectLength));
+            }           
+        }
+        else
+        {
+            interactingObject = null;
+        }
+
+        if (totalMoved >= parentObjectLength)
+        {
+            isCompleted = true;
+            Debug.Log("Turning action completed on " + gameObject.transform.parent.name);
+            Rigidbody gameObjectsRigidBody = parentObject.AddComponent<Rigidbody>();
+            gameObjectsRigidBody.useGravity = true;
+        }
+    }
+}
